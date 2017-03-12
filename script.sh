09863e6a7be6241bd634b833d8215b5647bcac80
demoonism <xianshanmingshi@msn.com>
2017-03-11 22:39:23 -0500

    Fix with respect to linux

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 echo "processing start!"
-python Parallel_JParser.py /mnt/f/Demoonism/DataScientist/Spark/data/batch1 /mnt/f/Demoonism/DataScientist/Spark/out/out_A &
-python Parallel_JParser.py /mnt/a/Spark/batch2 /mnt/f/Demoonism/DataScientist/Spark/out/out_B &
+##python Parallel_JParser.py /mnt/f/Demoonism/DataScientist/Spark/data/batch1 /mnt/f/Demoonism/DataScientist/Spark/out/out_A &
+##python Parallel_JParser.py /mnt/a/Spark/batch2 /mnt/f/Demoonism/DataScientist/Spark/out/out_B &
+
+python Parallel_JParser.py /mnt/1e69d2b1-91a9-473c-a164-db90daf43a3d/Backup_tw_2013_2_8/2013-02/2013-02-01 ../output/out
